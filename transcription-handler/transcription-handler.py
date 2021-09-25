f824efa26641858991470613b907e2012c8102ef
zvezdin <git@zvezd.in>
2021-09-26 01:15:19 +0200

    feat(rasa-integration): working integration and READMEs

--- transcription-handler/transcription-handler.py
+++ transcription-handler/transcription-handler.py
@@ -4,12 +4,13 @@ from flask import Flask, json, request
 from search_engine.src import ElasticSearchClient
 
 class TranscriptionHandler:
-    knownKeywords = ['python', 'working']
+    knownKeywords = [ "tinder", "issues", "discord", "bot", "java", "script", "wrapper", "emotion", "recognition", "pull", "request", "argus", "design", "document", "architecture"]
     rasaEndpoint = 'http://localhost:5005/model/parse'
 
     def __init__(self):    
         self.lastUser = None
         self.lastIsAction = False
+        self.es = ElasticSearchClient()
         
     def get_rasa_intent(self, m):
         payload = "{\"text\": \"" + m + "\"}"
@@ -25,7 +26,12 @@ class TranscriptionHandler:
         if self.lastIsAction:
             self.lastIsAction = False
             print("Expecting action, performing keyword search")
-            return self.do_keyword_search(self.get_keywords(m))
+            kwds = self.get_keywords(m)
+            res = self.do_keyword_search(kwds)
+            if res is not None:
+                return res
+            else:
+                return ""
         else:
             self.lastIsAction = self.is_intent_action(m)
             print("Action is ", self.lastIsAction)
@@ -35,18 +41,24 @@ class TranscriptionHandler:
         return sorted([x for x in self.knownKeywords if x.lower() in m.lower()])
 
     def do_keyword_search(self, keywords):
+        if len(keywords) == 0:
+            return None
         print("Searching with kwds", keywords)
-        return {'type': 'book', 'title': 'a great book'}
+        res = self.es.keyword_search(keywords)
+        print(res)
+        if len(res['hits']['hits']) == 0:
+            return None
+        return res['hits']['hits'][0]['_source']
 
 th = TranscriptionHandler()
 
 api = Flask(__name__)
 
-@api.route('/recommendation', methods=['POST'])
+@api.route('/recommendation/', methods=['POST'])
 def get_recommendation():
     m = str(request.get_data())
     print(m)
     return th.process_incoming_message(m)
 
-api.run()
+api.run(host="0.0.0.0")
 print("Listening...")
