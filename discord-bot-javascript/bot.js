a0f2a6db8babd8c07c75b801924c741db6352a40
Nikolay Pashov <nikifaets11@gmail.com>
2021-09-26 04:29:40 +0200

    debug: established communication with the other services

--- discord-bot-javascript/bot.js
+++ discord-bot-javascript/bot.js
@@ -3,6 +3,46 @@ const Discord = require("discord.js");
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
 
+/**
+ * promise-wrapped utility functions
+ */
+
+var readFile = path => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, file) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(file);
+            }
+        });
+    });
+};
+
+var writeFile = (path, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, data, err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+
+var readAudioBuffers = files => Promise.all(files.map(path => readFile(path)));
+
+var concactenatePcm = async files => {
+    const buffers = await readAudioBuffers(files);
+
+    const totalBufferLength = buffers
+        .map(buffer => buffer.length)
+        .reduce((total, length) => total + length);
+
+    return Buffer.concat(buffers, totalBufferLength);
+};
 /*
  DISCORD.JS VERSION 12 CODE
 */
@@ -12,6 +52,7 @@ const client = new Discord.Client();
 const speechClient = new speech.SpeechClient(); // https://www.npmjs.com/package/@google-cloud/speech https://cloud.google.com/docs/authentication/getting-started
 
 const config = require("./config.json");
+const { default: axios } = require("axios");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
@@ -22,85 +63,127 @@ const config = require("./config.json");
 }
 */
 
+let generateAudioFilename = (user) => {
+    return './recordings/' + user.id + '_' + Date.now() + '.pcm';
+}
+
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
-  client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
+    // This event will run if the bot starts, and logs in, successfully.
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
 });
 
 client.on("message", async message => {
-  if(message.author.bot) return; // Ignore other bots
-  if(!message.content.startsWith(config.prefix)) return; // Ignore messages not starting with our prefix
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-     
-  if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-  }
+    if (message.author.bot) return; // Ignore other bots
+    if (!message.content.startsWith(config.prefix)) return; // Ignore messages not starting with our prefix
 
-  if(command === "join") {
+    // Here we separate our "command" name, and our "arguments" for the command. 
+    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+    // command = say
+    // args = ["Is", "this", "the", "real", "life?"]
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    message.reply("MIKA VI DEBA");
-    if (!message.member.voice.channel) return message.reply('Please join a voice channel first!');
-    if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size > 0)) return message.reply(`I'm already in your voice channel!`);
-    
-    if (!message.member.voice.channel.joinable) return message.reply(`I don't have permission to join that voice channel!`);
-    if (!message.member.voice.channel.speakable) return message.reply(`I don't have permission to speak in that voice channel!`);
+    if (command === "ping") {
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    }
 
-    const connection = await message.member.voice.channel.join(); // https://discordjs.guide/voice/
-    await connection.play('ding.wav');
+    if (command === "join") {
 
-    connection.on('speaking', (user, speaking) => { // https://discord.js.org/#/docs/main/stable/class/VoiceConnection?scrollTo=speaking
-      if (!user) return; // If we don't know who is speaking
-      if (user.bot) return; // Don't listen to other bots
-      if (!speaking) return; // If for some reason they aren't actually speaking
-      console.log("Someone speaking");
-      const audio = connection.receiver.createStream(user, { mode: 'pcm' }); // Signed 16-bit PCM as the encoding, a Little-endian byte order, 2 Channels (Stereo) and a sample rate of 48000Hz. https://discordjs.guide/voice/receiving-audio.html#basic-usage
+        if (!message.member.voice.channel) return message.reply('Please join a voice channel first!');
+        if ((message.member.voice.channel.members.filter((e) => client.user.id === e.user.id).size > 0)) return message.reply(`I'm already in your voice channel!`);
 
-      const audioFileName = './recordings/' + user.id + '_' + Date.now() + '.pcm';
+        if (!message.member.voice.channel.joinable) return message.reply(`I don't have permission to join that voice channel!`);
+        if (!message.member.voice.channel.speakable) return message.reply(`I don't have permission to speak in that voice channel!`);
 
-      console.log("try to create file");
-      audio.pipe(fs.createWriteStream(audioFileName));
-      console.log("created file");
-      audio.on('end', async () => {
-        
-        console.log("End of speaking");
-        fs.stat(audioFileName, async (err, stat) => { // For some reason, Discord.JS gives two audio files for one user speaking. Check if the file is empty before proceeding
-          if (!err && stat.size) {
-            const file = fs.readFileSync(audioFileName);
-            console.log(file.byteLength);
-            const audioBytes = file.toString('base64');
-            const audio = {
-              content: audioBytes,
-            };
-            const config = {
-              encoding: 'LINEAR16',
-              sampleRateHertz: 48000,
-              languageCode: 'en-US',
-              audioChannelCount: 2,
-            };
-            const request = {
-              audio: audio,
-              config: config,
-            };
-            //const [response] = await speechClient.recognize(request);
-            /*const transcription = response.results
-              .map(result => result.alternatives[0].transcript)
-              .join('\n');*/
-            message.channel.send(file.byteLength + ". ");
-          }
+        const connection = await message.member.voice.channel.join(); // https://discordjs.guide/voice/
+        await connection.play('ding.wav');
+
+        var audioBytesLength = 0
+        var userBucket = {}
+
+        connection.on('speaking', (user, speaking) => { // https://discord.js.org/#/docs/main/stable/class/VoiceConnection?scrollTo=speaking
+
+            console.log(audioBytesLength)
+
+            if (!user) return; // If we don't know who is speaking
+            if (user.bot) return; // Don't listen to other bots
+            if (!speaking) return; // If for some reason they aren't actually speaking
+
+            const audio = connection.receiver.createStream(user, { mode: 'pcm' }); // Signed 16-bit PCM as the encoding, a Little-endian byte order, 2 Channels (Stereo) and a sample rate of 48000Hz. https://discordjs.guide/voice/receiving-audio.html#basic-usage
+
+            var oldName = null;
+            const audioFileName = audioBytesLength == 0 || oldName == null ? generateAudioFilename(user) : audioFileName;
+
+            if (userBucket[user.id] === undefined) {
+                userBucket[user.id] = []
+            }
+
+            userBucket[user.id].push(audioFileName);
+
+            console.log("null ? " + JSON.stringify(userBucket))
+            audio.pipe(fs.createWriteStream(audioFileName, { flags: 'a' }));
+
+            audio.on('end', async () => {
+
+                console.log("End of speaking");
+                fs.stat(audioFileName, async (err, stat) => { // For some reason, Discord.JS gives two audio files for one user speaking. Check if the file is empty before proceeding
+                    if (!err && stat.size) {
+                        const file = fs.readFileSync(audioFileName);
+                        audioBytesLength += file.byteLength;
+                        console.log(audioFileName + " : " + audioBytesLength);
+
+                        if (audioBytesLength <= 800000) {
+                            return;
+                        }
+
+                        audioBytesLength = 0
+                        oldName = audioFileName
+
+
+                        console.log(userBucket[user.id])
+
+                        let res = await concactenatePcm(userBucket[user.id])
+                        userBucket[user.id] = []
+
+                        writeFile('final_sending.pcm', res).then(async (res) => {
+
+                            const file = fs.readFileSync("final_sending.pcm");
+                            var audioBytes = file.toString("base64");
+
+                            const audio = {
+                                content: audioBytes,
+                            };
+                            const config = {
+                                encoding: 'LINEAR16',
+                                sampleRateHertz: 48000,
+                                languageCode: 'en-US',
+                                audioChannelCount: 2,
+                            };
+                            const request = {
+                                audio: audio,
+                                config: config,
+                            };
+
+                            const [response] = await speechClient.recognize(request);
+                            const transcription = response.results
+                                .map(result => result.alternatives[0].transcript)
+                                .join('\n');
+
+                            console.log(transcription)
+                            axios.post('http://10.15.3.0:5000/recommendation', transcription)
+                                .then(res => {
+                                    console.log("response: " + JSON.stringify(res.data))
+                                });
+                        })
+                    }
+                });
+            });
+
         });
-      });
+    }
 
-    });
-  }
-  
 });
 
 client.login(config.token);
